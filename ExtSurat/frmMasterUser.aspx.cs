de37ac28c063d694c8ef7c2b97ae4e508c6df0be
Trogalko <trogalko@gmail.com>
2013-01-21 08:50:00 +0700

    10

--- ExtSurat/frmMasterUser.aspx.cs
+++ ExtSurat/frmMasterUser.aspx.cs
@@ -37,9 +37,43 @@ namespace ExtSurat
         }
 
         [DirectMethod]
-        public void EditUser()
+        public void EditUser(string commandName, string userid)
         {
- 
+            taskManager1.StartAll();
+            HttpContext.Current.Session["isEditUser"] = false;            
+            HttpContext.Current.Session["isEditUser"] = false;
+
+            BusinessObjects.User u = new User();
+            //EDIT
+            if (u.LoadByPrimaryKey(userid.Trim()) && commandName.Trim() == "Edit")
+            {
+                var win = new Window()
+                {
+                    ID = "EditUserWindow",
+                    Title = "Edit User : " + u.Nama,
+                    Width = Unit.Pixel(800),
+                    Height = Unit.Pixel(600),
+                    Modal = true,
+                    AutoRender = false,
+                    Collapsed = false,
+                    Maximizable = false,
+                    Hidden = true,
+                    Draggable = false,
+                    Resizable = false,
+                    Closable = true
+                };
+
+                win.AutoLoad.Url = "~/frmUserWindowEdit.aspx?userid=" + userid.ToString().Trim();
+                win.AutoLoad.Mode = LoadMode.IFrame;
+                win.AutoLoad.ShowMask = true;
+                win.Render(this.Form);
+                win.Show();
+            }
+            else if (u.LoadByPrimaryKey(userid.Trim()) && commandName.Trim() == "Delete")
+            {
+                u.MarkAsDeleted();
+                u.AcceptChanges();
+            }
         }
 
         [DirectMethod]
