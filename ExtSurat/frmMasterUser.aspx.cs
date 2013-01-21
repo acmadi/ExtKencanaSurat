dec70ebc3fc1862fafe3c58851b49d9454b754a5
Trogalko <trogalko@gmail.com>
2013-01-21 08:43:24 +0700

    adding master user

--- ExtSurat/frmMasterUser.aspx.cs
+++ ExtSurat/frmMasterUser.aspx.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Ext.Net;
+using ExtSurat.BusinessObjects;
+using System.Data;
 
 namespace ExtSurat
 {
@@ -11,7 +14,38 @@ namespace ExtSurat
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!X.IsAjaxRequest)
+            {
+                this.storeUser.DataSource = GetUser();
+                this.storeUser.DataBind();
+            }
+        }
 
+        public DataTable GetUser()
+        {
+            UserQuery uQ = new UserQuery();
+            uQ.SelectAll();
+            DataTable dU = uQ.LoadDataTable();
+            return dU;
+        }
+
+        [DirectMethod]
+        public void storeUser_RefreshData(object sender, StoreRefreshDataEventArgs e)
+        {
+            this.storeUser.DataSource = GetUser();
+            this.storeUser.DataBind();
+        }
+
+        [DirectMethod]
+        public void EditUser()
+        {
+ 
+        }
+
+        [DirectMethod]
+        public void Refresh_Grid()
+        {
+ 
         }
     }
 }
